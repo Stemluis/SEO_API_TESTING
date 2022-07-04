018b71e16e6fa4e1919729a53a1e6ec357512f17
Luis Pagan <stemluis@gmail.com>
2022-07-04 23:14:55 +0000

    Modularization

--- YouTube.py
+++ YouTube.py
@@ -1,21 +1,45 @@
 import os
-import requests
+import sys
+import json
+import sqlalchemy as db
+import pandas as pd
 from pyyoutube import Api
 
-API_KEY = os.environ.get('YOUTUBE_API_KEY')
-api = Api(api_key=API_KEY)
+def main():
+    api = setupAPI()
+    query = "dogs"
+    videos = search(api, query, 5)
+    engine = addDatabase(query, videos)
+    printDatabase(engine, query)
 
-channel_id = "UC39a9uWnGIDYzi5BMndFmiw"
-channel_info = api.get_channel_info(channel_id=channel_id)
+def setupAPI(key=os.environ.get('YOUTUBE_API_KEY')):
+    """Returns an API object if the key is valid"""
+    api = Api(api_key=key)
+    try:
+        api.search_by_keywords(q="test")
+    except:
+        sys.exit("Invalid API key!")
+    return api
 
-playlist_id = channel_info.items[0].contentDetails.relatedPlaylists.uploads
+def search(api, query, num_videos=5):
+    """Returns a dictionary of size num_videos given an API object and search query"""
+    videos = api.search_by_keywords(q=query, search_type=['video'], count=num_videos, limit=num_videos)
+    videos = {
+        "videos" : [video.to_json() for video in videos.items]
+    }
+    return videos
 
-uploads_playlist_items = api.get_playlist_items(
-    playlist_id=playlist_id, count=10, limit=6
-)
+def addDatabase(query, videos):
+    """Returns a database engine containing the list of videos"""
+    engine = db.create_engine('sqlite:///videos.db')
+    database = pd.DataFrame.from_dict(videos)
+    database.to_sql((query + "_videos"), con=engine, if_exists='replace', index=False)
+    return engine
 
-videos = []
-for item in uploads_playlist_items.items:
-    videos.append(item.snippet.title)
+def printDatabase(engine, query):
+    """Prints the query table from the database"""
+    query_result = engine.execute(f"SELECT * FROM {query}_videos;").fetchall()
+    print(pd.DataFrame(query_result))
 
-print(videos)
+if __name__ == "__main__":
+    main()
